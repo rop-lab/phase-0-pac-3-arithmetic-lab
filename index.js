29f00891b1ba76e82dad3cca4e7c7ef056f4cdad
cheruiyotrop <cheruiyot.rop@student.moringaschool.com>
2023-12-30 17:34:23 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+function add(a, b) {
+    return a + b;
+}
+
+// Now, you can use the add() function
+let result = add(5, 3);
+console.log(result); // Output will be 8
+
+Number = 10;
+function add5(){
+    return(Number += 5);
+
+}
+function divideBy3(){
+    return (Number /=3);
+}
+divideBy3();
+add5();
+Number =10;
+add5();
+divideBy3();
+
+
